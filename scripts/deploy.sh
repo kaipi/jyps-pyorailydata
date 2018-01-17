cdec9cde4e221aa66d5a414b66e2d0a37b819fda
Timo Kaipiainen <timo.kaipiainen@sc5.io>
2018-01-17 23:36:41 +0200

    whops, not the whole app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case $1 in
   production)
-    rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ git@jyps.fi:/usr/share/nginx/pyorailydata.jyps.fi
+    rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build git@jyps.fi:/usr/share/nginx/pyorailydata.jyps.fi
     ;;
 esac
